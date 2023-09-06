1003fa1ee7a0b5d7a5660c19e2344f12d62515cb
aonemd <asalehmo7@gmail.com>
2023-09-06 21:51:38 +0200

    TAILWINDED!

--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
 import { count_days } from "how-many-days-haa";
 
 const daysEl = document.getElementById("days");
-daysEl.innerText = `${count_days()} days left`;
+daysEl.innerText = `${count_days()} days`;
