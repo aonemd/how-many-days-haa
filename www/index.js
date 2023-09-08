ad18a171eb05942716a5fff49a7f6c14dc9ce8a8
aonemd <asalehmo7@gmail.com>
2023-09-08 06:51:29 +0200

    Bubu

--- www/index.js
+++ www/index.js
@@ -2,3 +2,13 @@ import { count_days } from "how-many-days-haa";
 
 const daysEl = document.getElementById("days");
 daysEl.innerText = `${count_days()} days`;
+
+const fadeEl = document.getElementById("fade");
+
+daysEl.addEventListener('click', function() {
+  fadeEl.classList.add("opacity-100");
+}, false);
+
+daysEl.addEventListener('mouseout', function() {
+  fadeEl.classList.remove("opacity-100");
+}, false);
